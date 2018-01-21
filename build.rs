715987c4ca35ada2dfe36cfc0bfab58ffb8f9674
Joseph Murphy <air.jmurph@gmail.com>
2018-01-21 15:30:01 -0500

    Updated build script to take care of dependencies better.

--- build.rs
+++ build.rs
@@ -6,6 +6,9 @@ use std::path::{PathBuf};
 use std::process::Command;
 
 fn main() {
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    println!("cargo:rustc-link-search={:?}", out_path);
 
     let _output = Command::new("git")
 		.arg("submodule")
@@ -17,19 +20,19 @@ fn main() {
 		.expect("Failed to execute hostname command.");
 
     gcc::Build::new()
-        .file("./src/rpi_ws281x/mailbox.c")
-        .file("./src/rpi_ws281x/ws2811.c")
-        .file("./src/rpi_ws281x/pwm.c")
-        .file("./src/rpi_ws281x/pcm.c")
-        .file("./src/rpi_ws281x/dma.c")
-        .file("./src/rpi_ws281x/rpihw.c")
+        .file("src/rpi_ws281x/mailbox.c")
+        .file("src/rpi_ws281x/ws2811.c")
+        .file("src/rpi_ws281x/pwm.c")
+        .file("src/rpi_ws281x/pcm.c")
+        .file("src/rpi_ws281x/dma.c")
+        .file("src/rpi_ws281x/rpihw.c")
         .shared_flag(true)
         .compile("libws2811.so");
         
 
     // Tell cargo to tell rustc to link the system bzip2
     // shared library.
-    println!("cargo:rustc-link-lib=libws2811");
+    println!("cargo:rustc-link-lib=libws2811.so");
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -44,7 +47,6 @@ fn main() {
         .expect("Unable to generate bindings");
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
