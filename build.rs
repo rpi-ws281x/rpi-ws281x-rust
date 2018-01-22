cd9c872c8995ca66a6ecbbf46aaa88466990b167
Joseph Murphy <air.jmurph@gmail.com>
2018-01-22 13:05:39 -0500

    Added Travis CI, we'll see how that goes.

--- build.rs
+++ build.rs
@@ -32,7 +32,7 @@ fn main() {
 
     // Tell cargo to tell rustc to link the system bzip2
     // shared library.
-    println!("cargo:rustc-link-lib=libws2811.so");
+    //println!("cargo:rustc-link-lib=libws2811.so");
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
