f768574f807d203c5727e3d4b2121c7ed2d24c54
Joseph Murphy <air.jmurph@gmail.com>
2018-06-21 23:48:07 -0700

    Made some progress on the error handling, and getting closer overall.

--- src/channel/channel.rs
+++ src/channel/channel.rs
@@ -2,26 +2,58 @@ use std::slice::{from_raw_parts_mut}
 
 use palette::{LinSrgba, Rgba, Srgb, Linear};
 
-use super::super::util::{RawColor};
-use super::super::bindings::{ws2811_channel_t};
+use super::super::util::{RawColor, Result, WS2811Error};
+use super::super::bindings::{ws2811_channel_t, ws2811_render};
 
 #[derive(Deubg)]
 pub struct Channel {
-    leds: Vec<LinSrgba>,
-    raw_leds: &mut[RawColor],
     c_struct: ws2811_channel_t,
 }
 
 impl Channel {
-    pub unsafe fn new(c_struct: ws2811_channel_t) -> Channel<'a> {
-        let raw_leds = from_raw_parts_mut(c_struct.leds as *mut RawColor, c_struct.count as usize);
+    /// Build a channel from the C struct.
+    pub fn new(c_struct: ws2811_channel_t) -> Channel {
         Channel {
-            leds,
-            raw_leds,
             c_struct,
         }
     }
-    pub fn render(&mut self) {
+
+    /// Gets an immutable reference to the colors currently
+    /// on the string.  Let's go for immutable and mutable
+    /// versions first and see if that's too unergonomic.
+    ///
+    /// This function is moderately unsafe, it assumes that
+    /// the C library holds to its memory layout, which isn't
+    /// a total given.
+    pub fn leds() -> &[RawColor] {
+        unsafe {
+            return from_raw_parts(c_struct.leds as *RawColor, c_struct.count as usize);
+        }
+    }
+
+    /// Get a mutable reference to the leds themselves.
+    ///
+    /// This function is like moderately unsafe.  This assumes that
+    /// the C library actually holds to its memory layout.
+    pub fn leds_mut() -> &mut [RawColor] {
+        unsafe {
+            return from_raw_parts_mut(c_struct.leds as *mut RawColor, c_struct.count as usize);
+        }
+    }
+
+    /// Render the colors to the string.
+    ///
+    /// It doesn't automatically do this because it
+    /// is a somewhat costly operation that should 
+    /// be batched.
+    pub fn render(&mut self) -> Result<()> {
+        unsafe {
+            let ret: WS2811Error = ws2811_render(self.c_struct).into();
+            return match ret {
+                WS2811Error::Ok => Ok(()),
+                x => Err(x),
+            }
+        }
     }
 }
 
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -2,3 +2,6 @@ pub type RawColor = [u8; 4];
 
 mod strip_type;
 pub use strip_type::StripType;
+
+mod error;
+pub use error::{Result, WS2811Error};
